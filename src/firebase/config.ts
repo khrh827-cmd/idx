9f74bb8bce7c65e530678bdc83088ffe072ce3eb
Khaoula Rhouli <khrh827@vidalibarraquer.net>
2025-12-02 15:10:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5113010405-aee05",
+  "appId": "1:563124125538:web:569ef13391dfc816aaf63b",
+  "apiKey": "AIzaSyBIxkTXCgyywRVTKgern_8jXLIKNHKeYLg",
+  "authDomain": "studio-5113010405-aee05.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "563124125538"
+};
