f7203f6a77160b0e3f15730a4c04ff1b0c7b858d
Khaoula Rhouli <khrh827@vidalibarraquer.net>
2025-12-17 15:19:09 +0000

    puedes ponerme como logo e que esta en la carpeta public que se llama LO

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 
 export function Logo({ className }: { className?: string }) {
-  // Assuming the new logo file is named 'logo-nuevo.png' and placed in the /public directory.
-  // The user referred to "LOGO NUEVO (KHAOULARHOULI)". We'll use a sanitized name.
   return (
     <Image 
-      src="/logo-nuevo.png" 
+      src="/LOGO NUEVO (KHAOULARHOULI).png" 
       alt="Global Cargocare Logo" 
       width={48} 
       height={48} 
