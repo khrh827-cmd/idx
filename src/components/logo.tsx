6d4543624849cc2cc1bb979932d0064ca746337f
Khaoula Rhouli <khrh827@vidalibarraquer.net>
2026-01-19 16:57:56 +0000

    el meu logo es a la carpeta public i es diu "LOGO NUEVO (KHAOULARHOULI).

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image 
-      src="/logokhaoula.png" 
+      src="/LOGO NUEVO (KHAOULARHOULI).png" 
       alt="Global Cargocare Logo" 
       width={160} 
       height={48} 
