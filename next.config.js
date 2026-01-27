79e3054eba1037175238591cab7212ec2afda12a
Khaoula Rhouli <khrh827@vidalibarraquer.net>
2026-01-27 15:34:35 +0000

    en la portada te doy el enlace de el imgen y lo pones :https://www.savin

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'arcecon.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.savinodelbene.com',
+      },
     ],
   },
 };
