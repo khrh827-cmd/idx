a82e8d39bc20bebb9bf8bb22293f60ff266fa112
Khaoula Rhouli <khrh827@vidalibarraquer.net>
2026-01-27 14:52:39 +0000

    pon el logo pequeño que no salga del menu

--- app_old/src/components/logo.tsx
+++ app_old/src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo({ className }: { className?: string }) {
     <Image 
       src="/LOGO_K_R_MINI.png" 
       alt="Global Cargocare Logo" 
-      width={134} 
-      height={40} 
+      width={107} 
+      height={32} 
       className={className}
       unoptimized
     />
