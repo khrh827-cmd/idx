55eeef3f833cd9d56bb8d3c3ee789fb90a1a738a
Khaoula Rhouli <khrh827@vidalibarraquer.net>
2026-01-27 14:54:45 +0000

    encara més

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo({ className }: { className?: string }) {
     <Image 
       src="/LOGO_K_R_MINI.png" 
       alt="Global Cargocare Logo" 
-      width={80} 
-      height={24} 
+      width={60} 
+      height={18} 
       className={className}
       unoptimized
     />
